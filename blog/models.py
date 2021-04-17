8b35f574b1a913673d1af03d325053cd5afcb5c4
hahyunho <ha138972@gmail.com>
2021-04-17 14:27:14 +0900

    MY_HOME, first commit

---
+++ blog/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField(blank=True)
+    description = models.CharField('Description', max_length=100, blank=True, help_text="simple description")
+
+    def __str__(self):
+        return self.title
---
+++ blog/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import ListView
+from blog.models import Post
+
+# Create your views here.
+class PostLV(ListView):
+    model = Post
